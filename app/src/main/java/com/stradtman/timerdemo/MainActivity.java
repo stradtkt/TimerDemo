e97b9a8948d06b7d111ae14fad6fd817cd0bbd65
Kevin <stradtkt22@gmail.com>
2019-08-26 20:55:35 -0400

    Added items

--- app/src/main/java/com/stradtman/timerdemo/MainActivity.java
+++ app/src/main/java/com/stradtman/timerdemo/MainActivity.java
@@ -2,13 +2,76 @@ package com.stradtman.timerdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    SeekBar seekBar;
+    TextView timerTextView;
+    Boolean counterIsActive;
+    public void updateTimer(int secondsLeft) {
+        int minutes = (int) secondsLeft / 60;
+        int seconds = secondsLeft - minutes * 60;
+
+        String secondString = Integer.toString(seconds);
+        if(seconds <= 9) {
+            secondString = "0" + secondString;
+        }
+
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+    }
+    public void controlTimer(View view) {
+        if(counterIsActive == false) {
+            seekBar.setEnabled(false);
+            counterIsActive = true;
+            Log.i(TAG, "controlTimer: button pressed");
+            new CountDownTimer(seekBar.getProgress() * 1000 + 100, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    updateTimer((int) millisUntilFinished / 1000);
+                }
+
+                @Override
+                public void onFinish() {
+                    timerTextView.setText("0:00");
+                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.beep);
+                    mediaPlayer.start();
+                }
+            }.start();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        timerTextView = (TextView) findViewById(R.id.textView);
+        seekBar.setMax(600);
+        seekBar.setProgress(30);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                updateTimer(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
